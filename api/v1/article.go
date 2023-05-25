dde351018b85c92902eb8f21cb92bccc6ba78efd
Tommy <atgaopengju@163.com>
2023-05-25 18:17:46 +0800

    分页查询分类下的所有文章🍊

---
+++ api/v1/article.go
@@ -0,0 +1,84 @@
+package v1
+
+import (
+	"github.com/gin-gonic/gin"
+	"love_blog/model"
+	"love_blog/utils/errmsg"
+	"net/http"
+	"strconv"
+)
+
+// AddArt AddArticle 新增文章
+func AddArt(c *gin.Context) {
+	var articleData model.Article
+	_ = c.ShouldBindJSON(&articleData)
+	code := model.AddArt(&articleData)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    articleData,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
+
+// DelArtById 通过id删除指定文章
+func DelArtById(c *gin.Context) {
+	id := c.Param("id")
+	artId, err := strconv.Atoi(id)
+	if err != nil {
+		errmsg.BadRequest(c, "输入的id不合法")
+	}
+	code := model.DelArtById(artId)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
+
+// EdiArtById 根据id修改文章
+func EdiArtById(c *gin.Context) {
+	var data model.Article
+	_ = c.ShouldBindJSON(&data)
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		errmsg.BadRequest(c, "输入id不合法")
+	}
+	code := model.EdiArtById(id, &data)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
+
+// GetArtInfoById 根据id查询文章详情
+func GetArtInfoById(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	data, code := model.GetArtInfoById(id)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
+
+// GetArtInfoByCate 分页查询分类下的所有文章
+func GetArtInfoByCate(c *gin.Context) {
+	cid, _ := strconv.Atoi(c.Param("cid"))
+	pageSize, _ := strconv.Atoi(c.Param("pageSize"))
+	pageNum, _ := strconv.Atoi(c.Param("pageNum"))
+	switch {
+	case pageSize >= 100:
+		pageSize = 100
+	case pageSize < 0:
+		pageSize = 10
+	}
+	if pageNum <= 0 {
+		pageNum = 1
+	}
+	cateArtList, code, total := model.GetArtInfoByCate(cid, pageSize, pageNum)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    cateArtList,
+		"total":   total,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
--- model/Article.go
+++ model/Article.go
@@ -1,6 +1,9 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"love_blog/utils/errmsg"
+)
 
 type Article struct {
 	Category Category `gorm:"foreignkey:Cid"`
@@ -12,4 +15,63 @@ type Article struct {
 	Img          string `gorm:"type:varchar(100)" json:"img"`
 	CommentCount int    `gorm:"type:int;not null;default:0" json:"comment_count"`
 	ReadCount    int    `gorm:"type:int;not null;default:0" json:"read_count"`
+}
+
+// AddArt CreateAdd 新增文章
+func AddArt(articleAdd *Article) int {
+	err := db.Create(&articleAdd)
+	if err != nil {
+		return errmsg.ERROR //500
+	}
+	return errmsg.SUCCSE //200
+}
+
+// DelArtById 通过id删除指定文章
+func DelArtById(id int) int {
+	var article Article
+	err := db.Where("id = ?", id).Delete(&article).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCSE
+}
+
+// EdiArtById 根据id修改文章
+func EdiArtById(id int, data *Article) int {
+	var art Article
+	maps := make(map[string]interface{})
+	maps["title"] = data.Title
+	maps["cid"] = data.Cid
+	maps["img"] = data.Img
+	maps["content"] = data.Content
+	maps["desc"] = data.Desc
+	err := db.Model(&art).Where("id = ?", id).Updates(&maps).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCSE
+}
+
+// GetArtInfoById 根据id查询文章详情
+func GetArtInfoById(id int) (Article, int) {
+	var art Article
+	err := db.Where("id = ?", id).Preload("Category").First(&art).Error
+	if err != nil {
+		return art, errmsg.ERROR_ART_NOT_EXIST
+	}
+	// 更新浏览次数
+	db.Model(&art).Where("id = ?", id).UpdateColumn("read_count", gorm.Expr("read_count + ?", 1))
+	return art, errmsg.SUCCSE
+}
+
+// GetArtInfoByCate 分页查询分类下的所有文章
+func GetArtInfoByCate(cid int, pageSize int, pageNum int) ([]Article, int, int64) {
+	var cateArtList []Article
+	var total int64
+	err := db.Preload("Category").Limit(pageSize).Offset((pageNum-1)*pageSize).Where("cid = ?", cid).Find(&cateArtList).Error
+	db.Model(&cateArtList).Where("cid = ?", cid).Count(&total)
+	if err != nil {
+		return nil, errmsg.ERROR_CATE_NOT_EXIST, 0
+	}
+	return cateArtList, errmsg.SUCCSE, total
 }
