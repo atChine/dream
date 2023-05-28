a26397c2b3aa35e43ac45081ea26b477fda9eac8
Tommy <atgaopengju@163.com>
2023-05-28 22:32:59 +0800

    后台获取所有评论列表

--- api/v1/comment.go
+++ api/v1/comment.go
@@ -5,6 +5,7 @@ import (
 	"love_blog/model"
 	"love_blog/utils/errmsg"
 	"net/http"
+	"strconv"
 )
 
 // AddComment 新增评论
@@ -25,4 +26,72 @@ func GetCommentCount(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"total": count,
 	})
+}
+
+// GetCommentById 获取单个评论信息
+func GetCommentById(c *gin.Context) {
+	comId := c.Param("com_id")
+	comInfo, code := model.GetCommentById(comId)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data:":   comInfo,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
+
+// GetArtCommentList 展示页面获取评论列表
+func GetArtCommentList(c *gin.Context) {
+	artId := c.Param("artId")
+	pageSize, _ := strconv.Atoi(c.Query("pageSize"))
+	pageNum, _ := strconv.Atoi(c.Query("pageNum"))
+	switch {
+	case pageSize >= 100:
+		pageSize = 100
+	case pageSize <= 0:
+		pageSize = 10
+	}
+
+	if pageNum == 0 {
+		pageNum = 1
+	}
+	commentList, total, code := model.GetArtCommentList(artId, pageSize, pageNum)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    commentList,
+		"total":   total,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
+
+// DeleteCommentById 删除评论
+func DeleteCommentById(c *gin.Context) {
+	comId := c.Param("comId")
+	code := model.DeleteCommentById(comId)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
+
+// GetCommentList 后台获取所有评论列表
+func GetCommentList(c *gin.Context) {
+	pageSize, _ := strconv.Atoi(c.Query("pageSize"))
+	pageNum, _ := strconv.Atoi(c.Query("pageNum"))
+	switch {
+	case pageSize >= 100:
+		pageSize = 100
+	case pageSize <= 0:
+		pageSize = 10
+	}
+	if pageNum == 0 {
+		pageNum = 1
+	}
+	data, total, code := model.GetCommentList(pageSize, pageNum)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"total":   total,
+		"message": errmsg.GetErrMsg(code),
+	})
+
 }
--- model/Comment.go
+++ model/Comment.go
@@ -30,3 +30,59 @@ func GetCommentCount(artId string) int64 {
 	db.Find(&comment).Where("article_id = ?").Where("comment_status = ?", 1).Count(&count)
 	return count
 }
+
+// GetCommentById 获取单个评论信息
+
+func GetCommentById(comId string) (Comment, int) {
+	var comment Comment
+	err := db.Where("comment_id = ?", comId).First(&comment).Error
+	if err != nil {
+		return comment, errmsg.ERROR
+	}
+	return comment, errmsg.SUCCSE
+}
+
+// GetArtCommentList 展示页面获取评论列表
+func GetArtCommentList(artId string, pageSize int, pageNum int) ([]Comment, int64, int) {
+	var commentList []Comment
+	var total int64
+	db.Find(&Comment{}).Where("article_id = ?", artId).Where("comment_status = ?", 1).Count(&total)
+	err = db.Model(&Comment{}).Limit(pageSize).Offset((pageNum-1)*pageSize).
+		Order("Created_At DESC").
+		Select("comment.id, comment_id, article.article_title, user_id, article_id, user.user_name, "+
+			"comment_content, comment_status,comment.created_at,comment.deleted_at").
+		Joins("LEFT JOIN article ON comment.article_id = article.id").
+		Joins("LEFT JOIN user ON comment.user_id = user.id").Where("article_id = ?", artId).
+		Where("comment_status = ?", 1).Scan(&commentList).Error
+	if err != nil {
+		return commentList, 0, errmsg.ERROR
+	}
+	return commentList, total, errmsg.SUCCSE
+}
+
+// DeleteCommentById 删除评论
+func DeleteCommentById(comId string) int {
+	var comment Comment
+	err = db.Where("comment_id = ?", comId).Delete(&comment).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCSE
+}
+
+// GetCommentList 后台获取所有评论列表
+func GetCommentList(pageSize int, pageNum int) ([]Comment, int64, int) {
+	var commentList []Comment
+	var total int64
+	db.Find(&Comment{}).Count(&total)
+	err = db.Model(&commentList).Limit(pageSize).Offset((pageNum - 1) * pageSize).
+		Order("Created_At DESC").
+		Select("comment.id, comment_id, article.article_title, user_id, article_id, user.user_name, " +
+			"comment_content, comment_status,comment.created_at,comment.deleted_at").
+		Joins("LEFT JOIN article ON comment.article_id = article.id").
+		Joins("LEFT JOIN user ON comment.user_id = user.id").Scan(&commentList).Error
+	if err != nil {
+		return commentList, 0, errmsg.ERROR
+	}
+	return commentList, total, errmsg.SUCCSE
+}
