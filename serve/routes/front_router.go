5e47f33765594b833f3d866d61214ee99363ce66
Tommy <atgaopengju@163.com>
2023-06-01 11:51:24 +0800

    接通

---
+++ serve/routes/front_router.go
@@ -0,0 +1,36 @@
+package routes
+
+import (
+	"dream/serve/config"
+	"dream/serve/routes/middleware"
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+)
+
+// FrontRouter 前台页面接口路由
+func FrontRouter() http.Handler {
+	gin.SetMode(config.Cfg.Server.AppMode)
+
+	r := gin.New()
+	err := r.SetTrustedProxies([]string{"*"})
+	if err != nil {
+		return nil
+	}
+
+	r.Use(middleware.Cors()) // 跨域中间件
+	// 基于 cookies 存储 session
+	store := cookie.NewStore([]byte(config.Cfg.Session.Salt))
+
+	store.Options(sessions.Options{MaxAge: config.Cfg.Session.MaxAge})
+	r.Use(sessions.Sessions(config.Cfg.Session.Name, store)) // Session 中间件 (使用 Redis 存储引擎)
+
+	// 无需监权的接口
+	base := r.Group("/api/front")
+	{
+		base.POST("/login", userAuthAPI.Login) // 登录
+	}
+	return r
+}
--- serve/routes/router.go
+++ serve/routes/router.go
@@ -1,8 +1,41 @@
 package routes
 
+import (
+	"dream/serve/config"
+	"dream/serve/dao"
+	"dream/serve/utils"
+	"log"
+	"net/http"
+	"time"
+)
+
 // InitGlobalVariable 初始化全局变量
 func InitGlobalVariable() {
 
 	// 初始化数据库
+	dao.DB = utils.InitMySQLDB()
+}
 
+// BackendServer 后台服务
+//func BackendServer() *http.Server {
+//	backPort := config.Cfg.Server.BackPort
+//	log.Printf("后台服务启动于 %s 端口", backPort)
+//	return &http.Server{
+//		Addr:         backPort,
+//		Handler:      BackRouter(),
+//		ReadTimeout:  5 * time.Second,
+//		WriteTimeout: 10 * time.Second,
+//	}
+//}
+
+// FrontendServer 前台服务
+func FrontendServer() *http.Server {
+	frontPort := config.Cfg.Server.FrontPort
+	log.Printf("前台服务启动于 %s 端口", frontPort)
+	return &http.Server{
+		Addr:         frontPort,
+		Handler:      FrontRouter(),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
 }
