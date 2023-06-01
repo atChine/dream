b5f442654a38338363f4c431b48656976e143b6e
Tommy <atgaopengju@163.com>
2023-06-01 09:54:30 +0800

    友链

---
+++ serve/model/FriendLink.go
@@ -0,0 +1,10 @@
+package model
+
+// FriendLink 友链
+type FriendLink struct {
+	Universal
+	Name    string `gorm:"type:varchar(50)" json:"name"`
+	Avatar  string `gorm:"type:varchar(255)" json:"avatar"`
+	Address string `gorm:"type:varchar(255)" json:"address"`
+	Intro   string `gorm:"type:varchar(255)" json:"intro"`
+}
