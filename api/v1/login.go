e71da0122469337dd4f6bbaf3efa9f553f77a0b6
Tommy <atgaopengju@163.com>
2023-06-09 13:44:38 +0800

    后台登录

--- api/v1/login.go
+++ api/v1/login.go
@@ -1,10 +1,13 @@
 package v1
 
 import (
+	middleware "dream/middleare"
 	"dream/model"
 	"dream/utils/errmsg"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"time"
 )
 
 // LoginFront 前台登录
@@ -19,4 +22,56 @@ func LoginFront(c *gin.Context) {
 		"id":      formData.ID,
 		"message": errmsg.GetErrMsg(code),
 	})
+}
+
+// Login 后台登录
+func Login(c *gin.Context) {
+	var formData model.User
+	_ = c.ShouldBindJSON(&formData)
+	var token string
+	var code int
+	formData, code = model.CheckLogin(&formData)
+	if code == errmsg.SUCCSE {
+		setToken(c, formData)
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"status":  code,
+			"data":    formData.Username,
+			"id":      formData.ID,
+			"message": errmsg.GetErrMsg(code),
+			"token":   token,
+		})
+	}
+}
+
+// token生成函数
+func setToken(c *gin.Context, user model.User) {
+	j := middleware.NewJWT()
+	claims := middleware.MyClaims{
+		Username: user.Username,
+		StandardClaims: jwt.StandardClaims{
+			NotBefore: time.Now().Unix() - 100,
+			ExpiresAt: time.Now().Unix() + 604800,
+			Issuer:    "atGao",
+		},
+	}
+
+	token, err := j.CreateToken(claims)
+
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"status":  errmsg.ERROR,
+			"message": errmsg.GetErrMsg(errmsg.ERROR),
+			"token":   token,
+		})
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":  200,
+		"data":    user.Username,
+		"id":      user.ID,
+		"message": errmsg.GetErrMsg(200),
+		"token":   token,
+	})
+	return
 }
